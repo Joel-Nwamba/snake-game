5248a044a1d2da2a7d77fe17107f3480a1396646
Joel Nwamba <joel.elyon.nwamba@gmail.com>
2021-09-30 21:33:29 -0400

    allow snake to move forward

--- js/index.js
+++ js/index.js
@@ -2,7 +2,8 @@ const grid = document.querySelector(".grid");
 const start = document.getElementById("start");
 const score = document.getElementById("score");
 let squares = [];
-let currentSnake = [0,1,2];
+let currentSnake = [2,1,0];
+let direction = 1;
 
 function createGrid() {
     for(let i=0; i < 100; i++) {
@@ -21,5 +22,15 @@ createGrid();
 currentSnake.forEach(index => squares[index].classList.add("snake"));
 
 function move() {
-    
+    const tail = currentSnake.pop();
+    console.log(tail);
+    console.log(currentSnake);
+
+    squares[tail].classList.remove("snake");
+
+    currentSnake.unshift(currentSnake[0] + direction)
+    console.log(currentSnake)
+    squares[currentSnake[0]].classList.add("snake")
 }
+
+move();
