0b79693db0f0a1dffe6d5d891b4a7c13070a218f
Joel Nwamba <joel.elyon.nwamba@gmail.com>
2021-10-01 22:10:18 -0400

    Still unable to see a display of snake, must come back to it and fix the issue

--- js/index.js
+++ js/index.js
@@ -1,15 +1,17 @@
-const grid = document.querySelector(".grid");
-const start = document.getElementById("start");
-const score = document.getElementById("score");
+const grid = document.querySelector('.grid');
+const start = document.getElementById('start');
+const score = document.getElementById('score');
 let squares = [];
 let currentSnake = [2,1,0];
 let direction = 1;
+let width = 10;
+let appleIndex = 0;
 
 function createGrid() {
     for(let i=0; i < 100; i++) {
-        const square = document.createElement("div");
+        const square = document.createElement('div');
         //add class
-        square.classList.add("square");
+        square.classList.add('square');
         //Append to existing Div
         grid.appendChild(square);
         //push sqaure into squares
@@ -19,18 +21,54 @@ function createGrid() {
 }
 createGrid();
 
-currentSnake.forEach(index => squares[index].classList.add("snake"));
+currentSnake.forEach(index => squares[index].classList.add('snake'));
 
 function move() {
+
+if(
+ (currentSnake[0] +width >= 100 && direction === 100) ||
+ (currentSnake[0] % width === 9 && direction === 1) ||
+ (currentSnake[0] % width === 0 && direction === -1) ||
+ (currentSnake[0] - width < 0 && direction === -10) ||
+ squares[currentSnake[0]].classList.contains('snake')
+)
+return clearInterval(timerId);
+
     const tail = currentSnake.pop();
     console.log(tail);
     console.log(currentSnake);
 
-    squares[tail].classList.remove("snake");
+    squares[tail].classList.remove('snake');
 
     currentSnake.unshift(currentSnake[0] + direction)
     console.log(currentSnake)
-    squares[currentSnake[0]].classList.add("snake")
+    squares[currentSnake[0]].classList.add('snake');
 }
 
 move();
+// let timerId = setInterval(move, 1000);
+function generateApple() {
+    do {
+        appleIndex = Math.floor(Math.random() * squares.length )
+    } while(squares[appleIndex].classList.contains('snake'))
+    squares[appleIndex].classList.add('apple')
+}
+generateApple()
+
+
+
+function control(e) {
+    if (e.keyCode === 39) {
+        console.log('right pressed')
+        direction = 1
+    } else if (e.keyCode === 38) {
+        console.log('up pressed')
+        direction = -width;
+    } else if(e.keyCode === 37) {
+        console.log('left pressed')
+        direction = -1
+    } else {
+        console.log('down pressed')
+        direction = +width;
+    }
+}
